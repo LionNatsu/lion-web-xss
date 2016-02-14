c030fd15edf37f0c4e46ac66798b3db23684ba96
Lion Yang <lionxlion@foxmail.com>
2016-02-15 03:08:20 +0800

    output: for json and csv

--- controllers/router.js
+++ controllers/router.js
@@ -11,6 +11,7 @@ exports.DoBoom = function(app) {
     var ds = d.toUTCString();
     try {
       fs.appendFile('archive/' + req.params.key + '.log', ds + '\t' + req.ip + '\t' + req.body + '\n');
+      fs.appendFile('archive/' + req.params.key + '.json', ',' + req.body + '\n');
       log.info(ds + '\t' + req.params.key + '\t' + req.ip + '\t' + req.body);
     } catch (err) {
       log.err(err);
